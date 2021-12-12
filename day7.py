dc7562d587e07a9b2074dee741475d8d44f489ed
Dan Kay <dankaygit@gmail.com>
2021-12-12 20:47:48 +0100

    Day 7 cleaned

--- day7.py
+++ day7.py
@@ -3,33 +3,39 @@ importer = DataImporter("day7.csv", sep=",")
 positions = importer.read_single_line("int")
 print(positions)
 
-# PART A
-def calculate_fuel_consumption(positions, target_position):
-    total_consumption = 0
-    for sub in positions:
-        total_consumption += abs(target_position - sub)
-    return total_consumption
 
-min_fuel_consumption = calculate_fuel_consumption(positions, positions[0])
-for target_position in range(min(positions), max(positions)):
-    consumption = calculate_fuel_consumption(positions, target_position)
-    if consumption < min_fuel_consumption:
-        min_fuel_consumption = consumption
+def calculate_fuel_consumption(positions, target_position, part = 'A'):
+    """
 
-print(min_fuel_consumption)
-
-# PART B
-def calculate_new_fuel_consumption(positions, target_position):
+    :param positions:
+    :param target_position:
+    :param part:
+    :return:
+    """
     total_consumption = 0
     for sub in positions:
         N_steps = abs(target_position - sub)
-        total_consumption += int(N_steps*(N_steps+1)/2) # The whole trick of part B is an arithmetic sum of the steps
+        if part == 'A':
+            total_consumption += N_steps
+        else:
+            total_consumption += int(N_steps*(N_steps+1)/2)
     return total_consumption
 
-min_fuel_consumption = calculate_new_fuel_consumption(positions, positions[0])
-for target_position in range(min(positions), max(positions)):
-    new_consumption = calculate_new_fuel_consumption(positions, target_position)
-    if new_consumption < min_fuel_consumption:
-        min_fuel_consumption = new_consumption
 
-print(min_fuel_consumption)
+def find_min_fuel_consumption(positions, part = 'A'):
+    """
+
+    :param positions:
+    :return:
+    """
+    min_fuel_consumption = calculate_fuel_consumption(positions, positions[0], part = part)
+    for target_position in range(min(positions), max(positions)):
+        consumption = calculate_fuel_consumption(positions, target_position, part = part)
+        if consumption < min_fuel_consumption:
+            min_fuel_consumption = consumption
+    print (min_fuel_consumption)
+    return min_fuel_consumption
+
+
+find_min_fuel_consumption(positions, part = 'A')
+find_min_fuel_consumption(positions, part = 'B')
